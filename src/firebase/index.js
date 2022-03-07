0835bd28809f9ccd61de8db251aa18105c5ce199
Owoade <63651250+Owoade@users.noreply.github.com>
2022-03-07 09:04:37 +0100

    Update index.js

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,12 +2,6 @@ import {initializeApp} from 'firebase/app';
 import {getAuth} from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-//       apiKey: "AIzaSyBAJjkSqcg_dyZ8E4W9v7De_vba6r6Fwbg",
-//     authDomain: "sms360-8e9b3.firebaseapp.com",
-//     projectId: "sms360-8e9b3",
-//     storageBucket: "sms360-8e9b3.appspot.com",
-//     messagingSenderId: "932901977888",
-//     appId: "1:932901977888:web:3a35fb03d0f68c11eca145"
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId:process.env.REACT_APP_FIREBASE_PROJECT_ID,
