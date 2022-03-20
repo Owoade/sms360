81017c47c6cca1a4b6704c99e3e791c84f8e6cc2
Owoade <63651250+Owoade@users.noreply.github.com>
2022-03-20 21:21:12 +0100

    Update index.js

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,13 +2,15 @@ import {initializeApp} from 'firebase/app';
 import {getAuth} from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-    apiKey: "AIzaSyBAJjkSqcg_dyZ8E4W9v7De_vba6r6Fwbg",
-    authDomain: "sms360-8e9b3.firebaseapp.com",
-    projectId: "sms360-8e9b3",
-    storageBucket: "sms360-8e9b3.appspot.com",
-    messagingSenderId: "932901977888",
-    appId: "1:932901977888:web:3a35fb03d0f68c11eca145"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId:process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
+      
 };
+
 const app = initializeApp(firebaseConfig);
 const  db = getFirestore(app);
 const auth=getAuth(app);
